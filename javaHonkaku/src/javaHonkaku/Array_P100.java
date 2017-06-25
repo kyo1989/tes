66ac3cc94e1bc5e7bbd7060719fbeff063faf22f
kyo1989 <ak1s1w1sh@gmail.com>
2017-06-25 17:47:19 +0900

    21070625_2

---
+++ javaHonkaku/src/javaHonkaku/Array_P100.java
@@ -0,0 +1,12 @@
+package javaHonkaku;
+
+public class Array_P100 {
+	int[] arr1 = new int[10];//大きさ
+	int[] arr2 = {1,2,3};//自動でnewしてくれる
+////	int arr3 = new int[10];int arr3→int[] arr3としないとコンパイルエラー
+//
+//	int[] arr4;
+//	arr4 = new int[]{1,2};
+
+//	System.out.println(1);
+}
